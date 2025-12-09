6100eaac7fb28c3d84c0b3d52b8d9d160aeb1b5e
Wellinton Rubini Sartori <164983922+uSartori@users.noreply.github.com>
2025-12-09 10:29:51 -0300

    Comentários

--- colisao.asm
+++ colisao.asm
@@ -1,49 +1,45 @@
 ; colisao.asm
 .code
-; VERIFICA COLIS?O PLAYER x INIMIGOS
+
+; Verifica colisao player x inimigos
 verifica_colisao_player proc
-; X = player_x
-; Y = player_y
-; W = 29
-; H = 13
 
 checa_enemy1:
-    ; player_x < enemy1_x + 29
+    ; Player_x < enemy1_x + 29 (largura da sprite)
     mov ax, player_x
     mov bx, enemy1_x
     add bx, 29
     cmp ax, bx
     jge sem1
 
-    ; player_x + 29 > enemy1_x
+    ; Player_x + 29 > enemy1_x
     mov ax, player_x
     add ax, 29
     cmp ax, enemy1_x
     jle sem1
 
-    ; player_y < enemy1_y + 13
+    ; Player_y < enemy1_y + 13 (altura da sprite)
     mov ax, player_y
     mov bx, enemy1_y
     add bx, 13
     cmp ax, bx
     jge sem1
 
-    ; player_y + 13 > enemy1_y
+    ; Player_y + 13 > enemy1_y
     mov ax, player_y
     add ax, 13
     cmp ax, enemy1_y
     jle sem1
 
-    ; >>> COLIS?O DETECTADA <<<
+    ; Se a colisao for detectada, diminui uma vida e reposiciona a nave aliada
     dec qtd_vidas
     call reseta_nave_aliada 
     call perde_vida
     
 
 sem1:
-; =======================================================
-; CHECA COLIS?O COM INIMIGO 2
-; =======================================================
+
+; Checa a colisao com o inimigo 2 usando a mesma estrategia 
 checa_enemy2:
     mov ax, player_x
     mov bx, enemy2_x
@@ -72,9 +68,8 @@ checa_enemy2:
     call perde_vida
 
 sem2:
-; =======================================================
-; CHECA COLIS?O COM INIMIGO 3
-; =======================================================
+
+; Checa a colisao com o inimigo 2 usando a mesma estrategia     
 checa_enemy3:
     mov ax, player_x
     mov bx, enemy3_x
@@ -104,27 +99,28 @@ checa_enemy3:
 
 sem3:
     
+; Checa a colisao com o solo
 checa_solo:
 
-    mov ax, player_y     ; topo do player
-    add ax, 13           ; base do player (player_y + altura)
+    mov ax, player_y     ; Topo do player
+    add ax, 13           ; Base do player (player_y + altura)
 
-    cmp ax, 119          ; compara com Y do solo
+    cmp ax, 119          ; Compara com Y do solo
     jl sem4              
 
-    ; === COLIDIU COM O SOLO ===
+    ; Caso a nave colidir com o solo perde uma vida
     dec qtd_vidas
     call reseta_nave_aliada
     call perde_vida
-
+ 
 sem4:
-    ret
+    ret      ; Apos verificar todas as colisoes ele retorna
 
 verifica_colisao_player endp
 
 perde_vida proc
 
-    ; Sempre apaga a vida de cima (posi??o base)
+    ; Apaga todas as vidas sempre que tiver uma colisao  
     mov ax, 0
     mov dx, 181
     call calcula_posicao
@@ -144,8 +140,8 @@ perde_vida proc
     call desenha_vida
 
 
-    ; ---------- Se qtd_vidas == 2 ----------
-    cmp qtd_vidas, 2
+    ; Reescreve as sprites conforme a quantidade que o player ainda possuir de vidas 
+    cmp qtd_vidas, 2        ; Parte sempre de duas vidas, pois obrigatoriamente ele perdeu a terceira
     je verifica2
     cmp qtd_vidas, 1
     je verifica1
@@ -153,6 +149,7 @@ perde_vida proc
     je verifica0
     jmp fim_perde_vida
     
+; Se ele tiver 2 vidas, escreve a primeira e a segunda da esquerda para direita 
 verifica2:   
     mov ax, 0
     mov dx, 151
@@ -160,9 +157,8 @@ verifica2:
     mov bx, OFFSET vidas
     call desenha_vida
     
-
+; Se ele tiver uma vida escreve somente a vida da esquerda      
 verifica1:
-    ; ---------- Se qtd_vidas == 1 ----------
     mov ax, 0
     mov dx, 121
     call calcula_posicao
@@ -170,23 +166,23 @@ verifica1:
     call desenha_vida
     jmp fim_perde_vida
 
-
+; Se ele tiver 0 vidas, ele vai para a tela de Game Over
 verifica0:
-    ; ---------- Se qtd_vidas == 0 ----------
     cmp qtd_vidas, 0
     mov player_morto, 1
-    jmp game_over
+    call fase_inicio
 
 fim_perde_vida:
     ret
 
 perde_vida endp
 
+; Toda vez que a nave colidir ela reseta para as coordenadas Y = 90 e X = 10
 reseta_nave_aliada proc
     mov ax, player_y
     mov dx, player_x
     call calcula_posicao
-    mov bx, OFFSET sprite_vazio
+    mov bx, OFFSET sprite_vazio    ; Apaga a nave da coordenada anterior 
     call desenha_13x29
     mov player_x, 10  
     mov player_y, 90
